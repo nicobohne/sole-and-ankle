3a36adc7c9e2c29107ae8331f7434507987de090
Nico Bohne <20912141+nicobohne@users.noreply.github.com>
2023-01-30 19:05:52 +0100

    create shell

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -10,45 +10,53 @@ import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
 const ShoeIndex = ({ sortId, setSortId }) => {
-  return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={34} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
-  );
-};
+	return (
+		<Wrapper>
+			<MainColumn>
+				<Header>
+					<Title>Running</Title>
+					<Select label='Sort' value={sortId} onChange={ev => setSortId(ev.target.value)}>
+						<option value='newest'>Newest Releases</option>
+						<option value='price'>Price</option>
+					</Select>
+				</Header>
+				<Spacer size={34} />
+				<ShoeGrid />
+			</MainColumn>
+			<LeftColumn>
+				<Breadcrumbs>
+					<Breadcrumbs.Crumb href='/'>Home</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href='/sale'>Sale</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href='/sale/shoes'>Shoes</Breadcrumbs.Crumb>
+				</Breadcrumbs>
+				<Spacer size={42} />
+				<ShoeSidebar />
+			</LeftColumn>
+		</Wrapper>
+	)
+}
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: row-reverse;
+	align-items: baseline;
+	justify-content: flex-end;
+`
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+	flex: 1;
+	max-width: 248px;
+	margin-right: 32px;
+`
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+	flex: 4;
+`
 
-const Header = styled.header``;
+const Header = styled.header`
+	display: flex;
+	justify-content: space-between;
+`
 
 const Title = styled.h2`
   font-size: 1.5rem;
