284b6983f4a62ff56ca09e3d03d9f6139b402ae8
Nico Bohne <20912141+nicobohne@users.noreply.github.com>
2023-01-30 18:38:15 +0100

    create header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,75 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
+import { COLORS, WEIGHTS } from '../../constants'
+import Logo from '../Logo'
+import SuperHeader from '../SuperHeader'
 
 const Header = () => {
-  // Our site features two visual headers, but they should be
-  // grouped semantically as a single header.
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Logo />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-      </MainHeader>
-    </header>
-  );
-};
+	// Our site features two visual headers, but they should be
+	// grouped semantically as a single header.
+	return (
+		<header>
+			<SuperHeader />
+			<MainHeader>
+				<InnerHeader>
+					<Logo />
+					<Nav>
+						<NavLink href='/sale'>Sale</NavLink>
+						<NavLink href='/new'>New&nbsp;Releases</NavLink>
+						<NavLink href='/men'>Men</NavLink>
+						<NavLink href='/women'>Women</NavLink>
+						<NavLink href='/kids'>Kids</NavLink>
+						<NavLink href='/collections'>Collections</NavLink>
+					</Nav>
+				</InnerHeader>
+			</MainHeader>
+		</header>
+	)
+}
 
 const MainHeader = styled.div`
-  padding: 0 32px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
-`;
+	min-height: 72px;
+	padding: 0 32px;
+	border-bottom: 1px solid ${COLORS.gray[300]};
 
-const Nav = styled.nav``;
+	display: flex;
+	align-items: center;
+`
+
+const InnerHeader = styled.div`
+	display: flex;
+	align-items: baseline;
+	justify-content: space-between;
+	flex: 1;
+
+	&::after {
+		content: '';
+		display: block;
+	}
+
+	> :first-child,
+	&::after {
+		flex: 1;
+	}
+`
+
+const Nav = styled.nav`
+	display: flex;
+	gap: 48px;
+	margin-inline: 48px;
+`
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+	font-size: 1.125rem;
+	text-transform: uppercase;
+	text-decoration: none;
+	color: ${COLORS.gray[900]};
+	font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
-`;
+	&:first-of-type {
+		color: ${COLORS.secondary};
+	}
+`
 
-export default Header;
+export default Header
